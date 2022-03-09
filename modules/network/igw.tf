6356a443e8e5192f3bd17c2475af0dcd9b170173
max <maxsantos_d@hotmail.com>
2022-03-09 14:52:05 -0300

    creating the network infra

---
+++ modules/network/igw.tf
@@ -0,0 +1,24 @@
+#Criando internet gateway
+resource "aws_internet_gateway" "igw"{
+    vpc_id = aws_vpc.cluster_vpc.id  
+
+    tags = {
+        Name = format("%s-internet-gateway", var.cluster_name)
+    }
+}
+
+#Criando tabela de rota
+resource  "aws_route_table" "igw_route_table" {
+    vpc_id = aws_vpc.cluster_vpc.id  
+
+    tags = {
+        Name = format("%s-public-route", var.cluster_name)
+    }
+}
+
+#Criando rota e vinculando a tabela de rota
+resource "aws_route" "public_internet_access" {
+    route_table_id = aws_route_table.igw_route_table.id  
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id  
+}
